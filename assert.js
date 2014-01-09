cbc1f38879a02816baa15143cf680cb60f878e6c
Michiel Visser <michiel.visser@philips.com>
2014-01-09 20:59:31 +0100

    fix test code

--- assert.js
+++ assert.js
@@ -3,25 +3,25 @@
 	var _genAssertMessage = function(txt,level) {
 		console.log("Assert: "+txt+(new Error).stack.split("\n").slice(level||3).join("\n"));
 	}
-	function assert(expr,level) {
+	assert = function (expr,level) {
 		if (!expr) {
 			//throw("assert");
 			_genAssertMessage('',level);
 		}
 	}
-	function assert_equal(v1,v2,level) {
+	assert_equal = function(v1,v2,level) {
 		v1=JSON.stringify(v1);
 		v2=JSON.stringify(v2);
 		if (v1!=v2)
 			_genAssertMessage(''+(v1)+" != "+(v2),level);
 	}
 
-	function assert_reciprocal(func, arg,func2) {
+	assert_reciprocal = function(func, arg,func2) {
 		assert_equal(func2(func(arg)),arg,4);
 	}
 
-	function assert_reciprocalObject(constructor, arg,func) {
+	assert_reciprocalObject = function(constructor, arg,func) {
 		assert_equal(func.call(new constructor(arg)),arg,4);
 	}
-	
+
 })();
